1e651a909cc12cea9b402e696287a3e9ca0ebe84
Kiran <Kiran.one22@gmail.com>
2023-12-24 18:36:08 -0500

    Router update commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import "./App.css";
 import { Flex, MantineProvider } from "@mantine/core";
 import { Navbar } from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import { HashRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <MantineProvider>
       <Flex direction="column">
         <Navbar />
-        <Hero />
+        <HashRouter>
+          <Route index path="/" element={<Hero />}></Route>
+        </HashRouter>
       </Flex>
     </MantineProvider>
   );
