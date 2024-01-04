3cd0424322b90e09832d7f15819bc34c0808765e
Kiran <Kiran.one22@gmail.com>
2024-01-04 16:09:26 -0500

    #fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "@mantine/core/styles.css";
-import { MantineProvider, Image } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import Layout from "./components/Layout/Layout";
 
 function App() {
